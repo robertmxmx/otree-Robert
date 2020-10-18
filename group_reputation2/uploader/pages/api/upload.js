c629369c227f0be6d43f3cddfd843092b5581a57
honeybadger26 <honeybadger26@gmail.com>
2020-10-18 22:53:23 +1100

    user data validations

--- group_reputation2/uploader/pages/api/upload.js
+++ group_reputation2/uploader/pages/api/upload.js
@@ -15,19 +15,51 @@ export const config = {
 function stripContents(data) {
   var result = data;
   
+  // Remove header rows
   result.splice(0, 2);
-  result = result.map(elem => ({
-    birth_region:   parseInt(elem['Q4']),
-    other_br:       elem['Q4_15_TEXT'],
-    pi_q1:          parseInt(elem['Q6']),
-    pi_q2:          parseInt(elem['Q8']),
-    pi_q3:          parseInt(elem['Q9']),
-    pi_q4:          parseInt(elem['Q10']),
-    pi_q5:          parseInt(elem['Q11']),
-    pi_q6:          parseInt(elem['Q12']),
-    pi_q7:          parseInt(elem['Q13']),
-    pay_id:         elem['Q14']
-  }));
+
+  // Strip unnesecary values
+  result = result.map((e, index) => {
+    try {
+      return {
+        birth_region:   parseInt(e['Q4']),
+        other_br:       e['Q4_15_TEXT'],
+        pi_q1:          parseInt(e['Q6']),
+        pi_q2:          parseInt(e['Q8']),
+        pi_q3:          parseInt(e['Q9']),
+        pi_q4:          parseInt(e['Q10']),
+        pi_q5:          parseInt(e['Q11']),
+        pi_q6:          parseInt(e['Q12']),
+        pi_q7:          parseInt(e['Q13']),
+        pay_id:         e['Q14']
+      }
+    } catch {
+      throw new Error(`Error at row ${index+1}: Value expected to be an integer`)
+    }
+  });
+
+  // Ensure values are correct
+  result.forEach((e, index) => {
+    // Make sure birth region is between 1 and 15
+    if ((e.birth_region < 1) || (e.birth_region > 15)) {
+      throw new RangeError(`Error at row ${index+1}: Birth region out of range`);
+    }
+
+    // Make sure each pi question is between 1 and 7
+    const pi_labels = ['pi_q1', 'pi_q2', 'pi_q3', 'pi_q4', 
+      'pi_q5', 'pi_q6', 'pi_q7'];
+
+    pi_labels.forEach(p => {
+      if ((e[p] <  1) || (e[p] > 7)) {
+        throw new RangeError(`Error at row ${index+1}: ${p} is out of range`);
+      }
+    });
+
+    // Make sure pay_id is given
+    if (!e.pay_id) {
+      throw new ReferenceError(`Error at row ${index+1}: pay_id not given`);
+    }
+  });
   
   return {
     headers: result[0] ? Object.keys(result[0]) : [],
