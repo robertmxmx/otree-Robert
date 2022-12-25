6a9e4ee45a391e415ea2c22b069ba3a6317f3be2
Simon Schippl <honeybadger26@gmail.com>
2022-12-25 21:51:59 +1100

    Add tests for Group Reputation 3

--- group_reputation3/task1/tests.py
+++ group_reputation3/task1/tests.py
@@ -2,21 +2,54 @@ from otree.api import Currency as c, currency_range
 from . import pages
 from ._builtin import Bot
 from .models import Constants
-import random
-from otree.api import Submission
 
-# from _myshared import uploader
+
+# Test cases. Uncomment the specific case to use. NOTE: Remember to use the same
+# case in Task 2 tests
+CASE = "no_grouping"
+# CASE = "group_by_birth"
+# CASE = "group_by_politics"
+
+DEFAULT_SURVEY_ANSWERS = {
+    "birth_region": 3,
+    "pi_q1": 1,
+    "pi_q2": 1,
+    "pi_q3": 1,
+    "pi_q4": 1,
+    "pi_q5": 1,
+    "pi_q6": 1,
+    "pi_q7": 1
+}
 
 class PlayerBot(Bot):
 
+
     def play_round(self):
-        yield (pages.InitialSurvey, {
-            'birth_region': 3,
-            'pi_q1': 1,
-            'pi_q2': 1,
-            'pi_q3': 1,
-            'pi_q4': 1,
-            'pi_q5': 1,
-            'pi_q6': 1,
-            'pi_q7': 1
-        })
+
+        if CASE == "no_grouping":
+            yield (pages.InitialSurvey, DEFAULT_SURVEY_ANSWERS)
+        elif CASE == "group_by_birth":
+            if self.participant.id_in_session in [1, 5]:
+                yield (pages.InitialSurvey, DEFAULT_SURVEY_ANSWERS)
+            else:
+                yield (pages.InitialSurvey, {
+                    **DEFAULT_SURVEY_ANSWERS,
+                    'birth_region': 2
+                })
+        else:
+            if self.participant.id_in_session in [1, 5]:
+                yield (pages.InitialSurvey, {
+                    **DEFAULT_SURVEY_ANSWERS,
+                    'birth_region': 2
+                })
+            else:
+                yield (pages.InitialSurvey, {
+                    **DEFAULT_SURVEY_ANSWERS,
+                    'pi_q1': 7,
+                    'pi_q2': 7,
+                    'pi_q3': 7,
+                    'pi_q4': 7,
+                    'pi_q5': 7,
+                    'pi_q6': 7,
+                    'pi_q7': 7
+                })
--- group_reputation3/task2/tests.py
+++ group_reputation3/task2/tests.py
@@ -3,13 +3,26 @@ from . import pages
 from ._builtin import Bot
 from .models import Constants
 from otree.api import Submission
-import random
 
+# Test cases. Uncomment the specific case to use. NOTE: Remember to use the same
+# case in Task 1 tests
+CASE = "no_grouping"
+# CASE = "group_by_birth"
+# CASE = "group_by_politics"
+
+CHOSE_TO_TAKE = True
+DEDUCT_AMOUNT = 5
+WILL_SPEND = 5
+SAME_GROUPING_SHOULD_SPEND = 5
+SHOULD_SPEND = 5
+GENERAL_DEDUCTION = 5
+SAME_GROUPING_DEDUCTION = 5
+SHOULD_SPEND_GUESS = 5
 
 class PlayerBot(Bot):
 
     def play_round(self):
-        if 'group' not in self.participant.vars:
+        if "group" not in self.participant.vars:
             return
             
         if self.round_number == 1:
@@ -20,25 +33,98 @@ class PlayerBot(Bot):
             yield Submission(pages.Instructions3a, check_html=False)
             yield Submission(pages.Instructions4, check_html=False)
 
-            comp_answers = {'comp1': 1, 'comp2': True, 'comp3': self.session.config['deterrence'], 'comp5': 36, }
-            if self.session.config['rep_condition']:
-                comp_answers['comp4'] = 2
+            comp_answers = {
+                "comp1": 1,
+                "comp2": True,
+                "comp3": self.session.config["deterrence"],
+                "comp5": 36
+            }
+
+            if self.session.config["rep_condition"]:
+                comp_answers["comp4"] = 2
 
             yield (pages.Comprehension, comp_answers)
 
         yield pages.Commencement
 
         if self.player.role() == self.subsession.taking_player:
-            yield (pages.TakingDecision, {'chose_to_take': True})
+            yield (pages.TakingDecision, {"chose_to_take": CHOSE_TO_TAKE})
         elif self.player.role() == self.subsession.deducting_player:
-            yield Submission(pages.DeductingDecision, {'deduct_amount': 5}, check_html=False)
+            yield Submission(
+                pages.DeductingDecision,
+                {"deduct_amount": DEDUCT_AMOUNT},
+                check_html=False
+            )
         else:
             if self.round_number == 1:
-                yield (pages.WaitingDecision, {'will_spend': 5, 'should_spend': 5})
+                answers = {
+                    "will_spend": WILL_SPEND,
+                    "should_spend": SHOULD_SPEND
+                }
+
+                if CASE != "no_grouping":
+                    answers = {
+                        **answers,
+                        "same_grouping_should_spend": SAME_GROUPING_SHOULD_SPEND
+                    }
             else:
-                yield (pages.WaitingDecision, {'should_spend_guess': 5})
+                answers = { "general_deduction": GENERAL_DEDUCTION }
+
+                if CASE != "no_grouping":
+                    answers = {
+                        **answers, 
+                        "same_grouping_deduction": SAME_GROUPING_DEDUCTION,
+                        "should_spend_guess": SHOULD_SPEND_GUESS
+                    }
+
+            yield (pages.WaitingDecision, answers)
+
+        expected_payoff = Constants.initial_payoffs[self.player.role()]
+
+        if self.round_number == 1:
+            if (self.player.role() == "A") and CHOSE_TO_TAKE:
+                expected_payoff += (
+                    Constants.take_amount -
+                    (Constants.deduct["multiplier"] * DEDUCT_AMOUNT)
+                )
+            elif (self.player.role() == "B") and CHOSE_TO_TAKE:
+                expected_payoff -= Constants.take_amount + DEDUCT_AMOUNT
+            elif self.player.role() == "C":
+                if WILL_SPEND == DEDUCT_AMOUNT:
+                    expected_payoff += Constants.additional_amount
+
+                if (
+                    (CASE != "no_grouping") and
+                    (SHOULD_SPEND == SAME_GROUPING_SHOULD_SPEND)
+                ):
+                    expected_payoff += Constants.additional_amount
+        else:
+            if (self.player.role() == "A") and CHOSE_TO_TAKE:
+                expected_payoff += (
+                    Constants.take_amount -
+                    (Constants.deduct["multiplier"] * DEDUCT_AMOUNT)
+                )
+            elif self.player.role() == "B":
+                if DEDUCT_AMOUNT == GENERAL_DEDUCTION:
+                    expected_payoff += Constants.additional_amount
+
+                if CASE != "no_grouping":
+                    if DEDUCT_AMOUNT == SAME_GROUPING_DEDUCTION:
+                        expected_payoff += Constants.additional_amount
+
+                    if SHOULD_SPEND == SHOULD_SPEND_GUESS:
+                        expected_payoff += Constants.additional_amount
+            elif (self.player.role() == "C") and CHOSE_TO_TAKE:
+                expected_payoff -= Constants.take_amount + DEDUCT_AMOUNT
+
+        error_message = (
+            f"Player: {self.player.role()}, "
+            f"Actual Payoff: {self.player.payoff}, "
+            f"Expected Payoff: {c(expected_payoff)}"
+        )
+        assert self.player.payoff == c(expected_payoff), error_message
 
         yield pages.Feedback
 
-        if self.session.config['rep_condition'] and self.round_number == 2:
+        if self.session.config["rep_condition"] and self.round_number == 2:
             yield pages.CMessage
