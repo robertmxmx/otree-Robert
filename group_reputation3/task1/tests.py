53eab7efc22f0ef5c7e178c260188596e3c47253
Simon Schippl <honeybadger26@gmail.com>
2023-03-24 20:11:29 +1100

    Refactor tests
    - Update tests in line with new changes
    - Increase number of test players

--- group_reputation3/task1/tests.py
+++ group_reputation3/task1/tests.py
@@ -2,53 +2,63 @@ from . import pages
 from ._builtin import Bot
 
 
-# Test cases. Uncomment the specific case to use. NOTE: Remember to use the same
-# case in Task 2 tests
-# CASE = "no_grouping"
-# CASE = "group_by_birth"
-CASE = "group_by_politics"
+BR1 = { "birth_region": 1 }
+BR2 = { "birth_region": 2 }
 
-DEFAULT_SURVEY_ANSWERS = {
-    "birth_region": 3,
+PI1 = {
     "pi_q1": 1,
-    "pi_q2": 1,
-    "pi_q3": 1,
+    "pi_q2": 7,
+    "pi_q3": 7,
     "pi_q4": 1,
     "pi_q5": 1,
+    "pi_q6": 7,
+    "pi_q7": 7,
+}
+PI2 = {
+    "pi_q1": 7,
+    "pi_q2": 1,
+    "pi_q3": 1,
+    "pi_q4": 7,
+    "pi_q5": 7,
     "pi_q6": 1,
     "pi_q7": 1,
 }
+PI3 = {
+    "pi_q1": 4,
+    "pi_q2": 4,
+    "pi_q3": 4,
+    "pi_q4": 4,
+    "pi_q5": 4,
+    "pi_q6": 4,
+    "pi_q7": 4,
+}
 
+
+# NOTE: This test is not always successful at sorting by what is specifed in
+# the comments. This is due to a combination of oTree sorting bots by arrival
+# time and the bots landing on the wait page at indeterminate times.
 
 class PlayerBot(Bot):
     def play_round(self):
-        if CASE == "no_grouping":
-            yield (pages.InitialSurvey, DEFAULT_SURVEY_ANSWERS)
-        elif CASE == "group_by_birth":
-            if self.participant.id_in_session in [1, 5]:
-                yield (pages.InitialSurvey, DEFAULT_SURVEY_ANSWERS)
-            else:
-                yield (
-                    pages.InitialSurvey,
-                    {**DEFAULT_SURVEY_ANSWERS, "birth_region": 2},
-                )
-        else:
-            if self.participant.id_in_session in [1, 5]:
-                yield (
-                    pages.InitialSurvey,
-                    {**DEFAULT_SURVEY_ANSWERS, "birth_region": 2},
-                )
-            else:
-                yield (
-                    pages.InitialSurvey,
-                    {
-                        **DEFAULT_SURVEY_ANSWERS,
-                        "pi_q1": 7,
-                        "pi_q2": 7,
-                        "pi_q3": 7,
-                        "pi_q4": 7,
-                        "pi_q5": 7,
-                        "pi_q6": 7,
-                        "pi_q7": 7,
-                    },
-                )
+        # Sort this group by birth region
+        if self.participant.id_in_session == 1:
+            yield (pages.InitialSurvey, {**BR1, **PI3})
+        elif self.participant.id_in_session in [2, 3]:
+            yield (pages.InitialSurvey, {**BR2, **PI3})
+
+        # Sort this group by political ideology
+        elif self.participant.id_in_session == 4:
+            yield (pages.InitialSurvey, {**BR2, **PI1})
+        elif self.participant.id_in_session in [5, 6]:
+            yield (pages.InitialSurvey, {**BR2, **PI2})
+
+        # Sort this group randomly
+        elif self.participant.id_in_session in [7, 8, 9]:
+            yield (pages.InitialSurvey, {**BR2, **PI3})
+
+        # Also sort this group by birth region. This ensures at least one
+        # gets the extra belief questions
+        if self.participant.id_in_session == 10:
+            yield (pages.InitialSurvey, {**BR1, **PI3})
+        elif self.participant.id_in_session in [11, 12]:
+            yield (pages.InitialSurvey, {**BR2, **PI3})
--- group_reputation3/task2/tests.py
+++ group_reputation3/task2/tests.py
@@ -4,52 +4,96 @@ from ._builtin import Bot
 from .models import Constants
 from otree.api import Submission
 
-# Test cases. Uncomment the specific case to use. NOTE: Remember to use the same
-# case in Task 1 tests
-# CASE = "no_grouping"
-# CASE = "group_by_birth"
-CASE = "group_by_politics"
+from _myshared.constants import SortTypes
 
 CHOSE_TO_TAKE = True
 DEDUCT_AMOUNT = 5
 
-
-def get_bouns_answers(role, rep_condition, same_grouping):
-    answers = {}
-
-    if role == "A":
-        answers = {
+BONUS_ANSWERS = {
+    "A": {
+        "base": {
             "ee_a_session": 5,
             "ne_a": 5,
             "ne_a_b_session": 5,
             "ne_a_c_session": 5,
-        }
-
-        if same_grouping:
-            answers = {**answers, "ee_a_group": 5, "ne_a_b_group": 5}
-
-            if rep_condition:
-                answers = {**answers, "ne_a_c_group": 5}
-    elif role == "B":
-        answers = {
+        },
+        "same_grouping": {
+            "ee_a_group": 5,
+            "ne_a_b_group": 5,
+        },
+        "rep_condition": {
+            "ne_a_c_group": 5,
+        },
+    },
+    "B": {
+        "base": {
             "ee_b_session": 5,
             "ne_b": 5,
             "ne_b_b_session": 5,
             "ne_b_c_session": 5,
-        }
+        },
+        "same_grouping": {
+            "ee_b_group": 5,
+            "ne_b_b_group": 5,
+        },
+        "rep_condition": {
+            "ne_b_c_group": 5,
+        },
+    },
+    "C": {
+        "base": {
+            "ee_c_session": 5,
+            "ne_c": 5,
+            "ne_c_c_session": 5,
+        },
+        "same_grouping": {},
+        "rep_condition": {
+            "ee_c_group": 5,
+            "ne_c_c_group": 5,
+        },
+    },
+}
 
-        if same_grouping:
-            answers = {**answers, "ee_b_group": 5, "ne_b_b_group": 5}
 
-            if rep_condition:
-                answers = {**answers, "ne_b_c_group": 5}
-    elif role == "C":
-        answers = {"ee_c_session": 5, "ne_c": 5, "ne_c_c_session": 5}
+def get_bouns_answers(role, rep_condition, same_grouping):
+    role_answers = BONUS_ANSWERS[role]
+    answers = {**role_answers["base"]}
 
-        if rep_condition and same_grouping:
-            answers = {**answers, "ee_c_group": 5, "ne_c_c_group": 5}
+    if same_grouping:
+        answers = {**answers, **role_answers["same_grouping"]}
+
+        if rep_condition:
+            answers = {**answers, **role_answers["rep_condition"]}
 
     return answers
+
+"""
+Return a list of sort types where these sort types have more than 1 group.
+For example: if there are 2 birth region groups and 1 political ideology group
+this array will return: ["birth_region"]
+"""
+def get_same_groupings(players):
+    birth_region_players = 0
+    political_ideology_players = 0
+    same_groupings = []
+    
+    for player in players:
+        sorted_by = player.participant.vars["sorted_by"]
+
+        if sorted_by == SortTypes.NONE.value:
+            continue
+        elif sorted_by == SortTypes.BIRTH_REGION.value:
+            birth_region_players += 1
+        elif sorted_by == SortTypes.POLITICAL_IDEOLOGY.value:
+            political_ideology_players += 1
+
+    if birth_region_players > Constants.players_per_group:
+        same_groupings.append(SortTypes.BIRTH_REGION.value)
+
+    if political_ideology_players > Constants.players_per_group:
+        same_groupings.append(SortTypes.POLITICAL_IDEOLOGY.value)
+
+    return same_groupings
 
 
 class PlayerBot(Bot):
@@ -58,8 +102,9 @@ class PlayerBot(Bot):
             return
 
         rep_condition = self.session.config["rep_condition"]
+        round_num = self.round_number
 
-        if self.round_number == 1:
+        if round_num == 1:
             yield Submission(pages.Instructions, check_html=False)
             yield Submission(pages.Instructions2)
             yield Submission(pages.VideoInstructions, check_html=False)
@@ -70,7 +115,7 @@ class PlayerBot(Bot):
             comp_answers = {
                 "comp1": 1,
                 "comp2": True,
-                "comp3": self.session.config["deterrence"],
+                "comp3": 1 if self.session.config["deterrence"] else 3,
                 "comp5": 36,
             }
 
@@ -90,8 +135,10 @@ class PlayerBot(Bot):
                 check_html=False,
             )
 
-        if self.round_number == 1:
-            same_grouping = CASE != "no_grouping"
+        if round_num == 1:
+            same_groupings = get_same_groupings(self.subsession.get_players())
+            same_grouping = self.participant.vars["sorted_by"] in same_groupings
+
             answers = get_bouns_answers(
                 self.player.role(), rep_condition, same_grouping
             )
@@ -100,31 +147,31 @@ class PlayerBot(Bot):
 
         expected_payoff = Constants.initial_payoffs[self.player.role()]
 
-        # TODO: Normal payoff and Bonus question payoff
-        if self.player.role() == "A":
-            # if CHOSE_TO_TAKE:
-            #     expected_payoff += Constants.take_amount - (
-            #         Constants.deduct["multiplier"] * DEDUCT_AMOUNT
-            #     )
-
-            expected_payoff = None
-        elif self.player.role() == "B":
-            # if CHOSE_TO_TAKE:
-            #     expected_payoff -= Constants.take_amount + DEDUCT_AMOUNT
-
-            expected_payoff = None
-        elif self.player.role() == "C":
-            expected_payoff = None
-
-        if expected_payoff is not None:
-            error_message = (
-                f"Player {self.player.role()} - "
-                f"Actual Payoff: {self.player.payoff}, "
-                f"Expected Payoff: {c(expected_payoff)}"
+        # Calculate normal payoffs
+        if self.player.role() == "A" and CHOSE_TO_TAKE:
+            expected_payoff += Constants.take_amount - (
+                Constants.deduct["multiplier"] * DEDUCT_AMOUNT
             )
-            assert self.player.payoff == c(expected_payoff), error_message
+        elif CHOSE_TO_TAKE and (
+            (round_num == 1 and self.player.role() == "B")
+            or (round_num == 2 and self.player.role() == "C")
+        ):
+            expected_payoff -= Constants.take_amount + DEDUCT_AMOUNT
+
+        # TODO: Needs to be more specific.
+        # e.g. ee_c_session == playerB.deduct_amount
+        q_to_pay = self.player.field_maybe_none("bonus_q_to_pay")
+        if self.player.field_maybe_none(q_to_pay) == 5:
+            assert self.player.bonus_paid == True
+
+        error_message = (
+            f"Player {self.player.role()} - "
+            f"Actual Payoff: {self.player.final_payoff}, "
+            f"Expected Payoff: {c(expected_payoff)}"
+        )
+        assert self.player.final_payoff == c(expected_payoff), error_message
 
         yield pages.Feedback
 
-        if rep_condition and self.round_number == 2:
+        if rep_condition and round_num == 2:
             yield pages.CMessage
