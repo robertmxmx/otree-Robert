a45ed398619e9a3e6f527fc1f4c797797d9c1578
Toby <toby.handfield@gmail.com>
2023-08-08 14:59:54 +1000

    alternative tests

--- group_reputation3/task1/tests.py
+++ group_reputation3/task1/tests.py
@@ -1,22 +1,23 @@
 from . import pages
 from ._builtin import Bot
 import random
+# random.seed(12)
 
-BR1 = { "birth_region": 1 }        
-BR2 = { "birth_region": 2 }
+BR1 = { "birth_region": 5 }        
+BR2 = { "birth_region": 5 }
 
 PI1 = {
-    "pi_q1": 1,
-    "pi_q2": 7,
-    "pi_q3": 7,
-    "pi_q4": 1,
-    "pi_q5": 1,
+    "pi_q1": 7,
+    "pi_q2": 4,
+    "pi_q3": 5,
+    "pi_q4": 7,
+    "pi_q5": 5,
     "pi_q6": 7,
     "pi_q7": 7,
 }
 PI2 = {
     "pi_q1": 7,
-    "pi_q2": 1,
+    "pi_q2": 4,
     "pi_q3": 1,
     "pi_q4": 5,
     "pi_q5": 7,
@@ -28,7 +29,7 @@ PI3 = {
     "pi_q2": 4,
     "pi_q3": 3,
     "pi_q4": 4,
-    "pi_q5": 4,
+    "pi_q5": 2,
     "pi_q6": 4,
     "pi_q7": 4,
 }
@@ -55,25 +56,25 @@ class PlayerBot(Bot):
 
         # Sort by random input
         if die == 1:
-            inputs = {**BR1, **PI1}
+            inputs = {**BR1, **PI2}
         if die == 2:
-            inputs = {**BR2, **PI1}
+            inputs = {**BR2, **PI2}
         if die == 3:
-            inputs = {"birth_region": random.randint(1, 14), **PI2}
+            inputs = {"birth_region": random.randint(2, 13), **PI1}
         if die == 4:
-            inputs = {"birth_region": random.randint(1, 7), **PI1}
+            inputs = {"birth_region": random.randint(2, 13), **PI3}
         if die == 5:
-            inputs = {"birth_region": random.randint(1, 7), **PI4}
+            inputs = {"birth_region": random.randint(5, 7), **PI4}
         if die == 6:
-            inputs = {"birth_region": random.randint(1, 3), **PI3}
+            inputs = {"birth_region": random.randint(4, 9), **PI3}
         if die == 7:
-            inputs = {"birth_region": random.randint(1, 3), **PI3}
+            inputs = {"birth_region": random.randint(1, 7), **PI4}
         if die == 8:
-            inputs = {"birth_region": random.randint(1, 12), **PI2}
+            inputs = {"birth_region": random.randint(4, 12), **PI3}
         if die == 9:
-            inputs = {"birth_region": random.randint(3, 7), **PI1}
+            inputs = {"birth_region": random.randint(4, 7), **PI4}
         if die == 10:
-            inputs = {"birth_region": random.randint(3, 7), **PI4}
+            inputs = {"birth_region": random.randint(6, 7), **PI2}
         # # Sort this group by birth region
         # if id == 1:
         #     inputs = {**BR2, **PI3}
